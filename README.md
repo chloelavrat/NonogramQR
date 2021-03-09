a09a6777894b67fb3187e3078e30a4b9efec3f93
azerty <75258525+azerty-waves@users.noreply.github.com>
2021-03-09 19:05:49 +0100

    Main

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # QR-Gram
-Converter: from QR code to nonograms.
+> Converter: from QR code to nonograms.
+
+I like nonograms and I like QR-codes. Both open the door to a new word when they are scanned or completed. That's why I created a fast and dirty python code that transfers a string of characters into a QR-code which is then encoded into a nonogram. 
+
+To access the message (or website), the player must first complete the nonogram and then scan it. Obviously, the longer the message, the more complicated the nonogram is.
+
+## Example
+
+So for QR-code:
+
+![qrcode](README.assets/qrcode.png)
+
+You have the nonogram:
+
+```
+                      1 1                                
+                    1 3 3             1         1 1      
+                    3 1 1         3 3 1 4       3 3 1    
+                  1 1 1 2 1 7   2 1 1 1 3   7   1 1 3 1  
+                  1 2 2 1 1 1   1 2 1 1 1   1   2 1 1 1 7
+                7 2 1 1 1 2 1 1 1 3 1 1 1 2 4 1 2 3 1 1 2
+                3 1 3 3 3 1 1 1 3 1 2 2 3 2 1 1 1 3 3 1 1
+                7 1 1 1 1 1 7 1 2 1 2 2 1 2 2 4 1 1 5 2 2
+        7 2 1 7
+      1 1 4 1 1
+1 3 1 3 1 1 3 1
+1 3 1 1 1 1 3 1
+1 3 1 1 1 1 3 1
+        1 1 1 1
+      7 1 1 1 7
+            1 1
+      2 2 2 1 2
+      2 1 1 1 1
+  3 4 1 1 1 1 1
+    1 3 1 4 2 1
+      1 1 4 1 3
+        3 1 2 1
+      7 1 2 4 1
+      1 1 1 1 3
+1 3 1 1 2 2 2 1
+    1 3 1 1 2 3
+    1 3 1 2 3 3
+    1 1 2 2 1 3
+          7 2 2
+```
+
+## Sources
+
+[Reddit - r/dailyprogrammer]( https://www.reddit.com/r/dailyprogrammer/comments/42lhem/20160125_challenge_251_easy_create_nonogram/)
+
+[Reddit - TheBlackCat13]( https://www.reddit.com/user/TheBlackCat13/)
+
+[PyPi - PyQRCode](https://pypi.org/project/PyQRCode/)
---
+++ qr-gram.py
@@ -0,0 +1,89 @@
+#----------------------------------------------------------
+# Name: QR-Gram
+# Autor: azerty-waves
+# Site: azerty-waves.github.io
+# Comments: 
+# from the previous work of the redditor 
+# r/TheBlackCat13/
+#----------------------------------------------------------
+from itertools import zip_longest
+import pyqrcode
+
+def get_qrcode(string):
+    qr = pyqrcode.create(string)
+    bin = qr.text(quiet_zone=0)
+    bin = bin.replace('1' , '*')
+    bin = bin.replace('0' , ' ')
+    return bin[:-1]
+
+def print_qrcode(string):
+    qr = pyqrcode.create(string)
+    print(qr.terminal(quiet_zone=1))
+    pass
+
+def get_counts(pic):
+    """Gets the counts of the items in the rows and columns.
+
+    `pic` is a multi-line string, where each line is a row."""
+    rows = pic.split('\n')
+    cols = (''.join(x) for x in zip_longest(*rows, fillvalue=' '))
+    count = lambda lines: [[str(len(y)) for y in x.split()] for x in lines]
+    return count(cols), count(rows)
+
+def format_count(counts):
+    """Get the right number of elements for each count."""
+    maxnum = max(len(x) for x in counts)
+    maxlet = max(max(len(y) for y in x) for x in counts)
+    counts = ([y.rjust(maxlet) for y in x] for x in counts)
+
+    maxspc = ' '*maxlet
+    return [[maxspc]*(maxnum-len(x)) + x for x in counts]
+
+def print_counts(pic):
+    """Print the results in the format requested by the question."""
+    colcounts, rowcounts = get_counts(pic)
+
+    colcounts = format_count(colcounts)
+    rowcounts = format_count(rowcounts)
+
+    rowcounts = [' '.join(x) for x in rowcounts]
+
+    colpad = ' '*(len(rowcounts[0])+1)
+    colcounts = (' '.join(x) for x in zip(*colcounts))
+    colcounts = (colpad+x for x in colcounts)
+
+    print(*colcounts, sep='\n')
+    print(*rowcounts, sep='\n')
+
+def export_csv(pic, filename):
+    """Export the result into CSV"""
+
+    f= open(filename+".csv","w+")
+    colcounts, rowcounts = get_counts(pic)
+
+    mod = len(colcounts)+len(rowcounts)
+    colcounts = format_count(colcounts)
+    rowcounts = format_count(rowcounts)
+
+    rowcounts = [';'.join(x) for x in rowcounts]
+
+    colpad = ';'*(len(rowcounts[0])+1)
+    colcounts = (';'.join(x) for x in zip(*colcounts))
+    colcounts = (colpad+x for x in colcounts)
+
+    for c in colcounts:
+        f.write(c+";")
+        f.write('\n')
+    for r in rowcounts:
+        f.write(r+";")
+        f.write('\n')
+    f.close()
+
+#######################################################
+
+string = ":)"
+
+pic_2 = get_qrcode(string)
+print_qrcode(string)
+print_counts(pic_2)
+export_csv(pic_2,"my-nono")
